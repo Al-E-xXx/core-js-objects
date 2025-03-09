b33042b501367e82a41749ed6af8f4bc14cd3e74
Al-E-xXx <vurdalackoff@gmail.com>
2025-03-09 21:00:28 +0300

    feat: solve second task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,20 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects.reduce(
+    (acc, obj) =>
+      Object.assign(
+        acc,
+        Object.fromEntries(
+          Object.entries(obj).map(([key, value]) => [
+            key,
+            (acc[key] || 0) + value,
+          ])
+        )
+      ),
+    {}
+  );
 }
 
 /**
