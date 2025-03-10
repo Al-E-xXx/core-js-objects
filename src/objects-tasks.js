35b0522ddd219a2c1b91fe842ca135aff7cfe5a9
Al-E-xXx <vurdalackoff@gmail.com>
2025-03-10 18:35:17 +0300

    feat: solve twelfth task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -265,8 +265,14 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  const result = arr.slice();
+
+  result.sort(
+    (a, b) => a.country.localeCompare(b.country) || a.city.localeCompare(b.city)
+  );
+
+  return result;
 }
 
 /**
